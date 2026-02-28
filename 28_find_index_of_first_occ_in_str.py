264c469b76e3f1a0b4aa9259e81efc76d165853f
ADHITHI.R <adhithi.r@Adhithi.local>
2026-02-28 12:19:49 +0530

    28

---
+++ 28_find_index_of_first_occ_in_str.py
@@ -0,0 +1,29 @@
+"""
+LeetCode Problem: Find the Index of the First Occurence in a String
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+"""
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        if needle in haystack:
+            return haystack.index(needle)
+        return -1
