e5175e64146178548e9dacfe05c8e266177b0202
ADHITHI.R <adhithi.r@Adhithi.local>
2026-03-09 16:24:10 +0530

    387

---
+++ 387_first_unique_ch_in_str.py
@@ -0,0 +1,37 @@
+"""
+LeetCode Problem: First Unique Character in a String
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Explanation:
+The character 'l' at index 0 is the first character that does not occur at any other index.
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+"""
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for i in s:
+            if s.count(i)==1:
+                d=s.index(i)
+                return d
+                break
+        else:
+            return -1
