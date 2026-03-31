2d0285d8adf8c0bfafeb64849e33571125fda6ef
ADHITHI.R <adhithi.r@Adhithi.local>
2026-03-31 09:41:15 +0530

    204

---
+++ 204_count_primes.py
@@ -0,0 +1,40 @@
+"""
+Leetcode Problem: Count Primes
+
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+Example 2:
+
+Input: n = 0
+Output: 0
+
+Example 3:
+
+Input: n = 1
+Output: 0
+ 
+Constraints:
+
+0 <= n <= 5 * 106
+"""
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+
+        prime = [True] * n
+        prime[0] = prime[1] = False
+
+        for i in range(2, int(n**0.5) + 1):
+            if prime[i]:
+                for j in range(i*i, n, i):
+                    prime[j] = False
+
+        return sum(prime)
